c9d1abbeba596980a76ed1161f7c1d22f63426f8
Paulo Ferrari <pf.devcon@gmail.com>
2021-02-09 02:09:42 -0300

    added encrypt password functionalitie

--- src/services/UsersServices/CreateUser.service.ts
+++ src/services/UsersServices/CreateUser.service.ts
@@ -1,4 +1,5 @@
 import { getRepository } from "typeorm";
+import { hash } from "bcryptjs";
 
 import User from "src/models/Users";
 
@@ -8,8 +9,10 @@ interface Request {
   password: string;
 }
 
+interface Response extends Omit<User, "password"> {}
+
 class CreateUserService {
-  public async run({ name, email, password }: Request): Promise<User> {
+  public async run({ name, email, password }: Request): Promise<Response> {
     const userRepository = getRepository(User);
 
     const checkUserExists = await userRepository.findOne({ where: { email } });
@@ -18,11 +21,25 @@ class CreateUserService {
       throw new Error("Email address already used");
     }
 
-    const user = userRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await userRepository.save(user);
 
-    return user;
+    const userResponse: Response = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return userResponse;
   }
 }
 
